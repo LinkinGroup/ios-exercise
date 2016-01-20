84944283227537139d260803617265ec9191ca6c
Linkinzoo <ztzhengbc@163.com>
2016-01-21 02:51:41 +0800

    初始化项目

---
+++ ios-exercise/ios-exercise/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  ios-exercise
+//
+//  Created by 郑博辰 on 16/1/21.
+//  Copyright © 2016年 com.link. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ ios-exercise/ios-exercise/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  ios-exercise
+//
+//  Created by 郑博辰 on 16/1/21.
+//  Copyright © 2016年 com.link. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
